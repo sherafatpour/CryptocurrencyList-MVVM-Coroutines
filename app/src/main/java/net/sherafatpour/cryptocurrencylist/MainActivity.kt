c49164048efa607c486abd59074d7a7950006d4f
Reza sherafatpour <reza.sharafatpour@gmail.com>
2022-04-12 17:16:29 +0430

    ViewBinding was enabled

--- app/src/main/java/net/sherafatpour/cryptocurrencylist/MainActivity.kt
+++ app/src/main/java/net/sherafatpour/cryptocurrencylist/MainActivity.kt
@@ -3,11 +3,17 @@ package net.sherafatpour.cryptocurrencylist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
+import net.sherafatpour.cryptocurrencylist.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+       binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
     }
 }
