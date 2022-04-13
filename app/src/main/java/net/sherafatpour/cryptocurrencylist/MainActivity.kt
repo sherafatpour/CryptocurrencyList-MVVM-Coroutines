452570a51633d74f39a55eaccb5f2336fd34f20c
Reza sherafatpour <reza.sharafatpour@gmail.com>
2022-04-13 11:28:37 +0430

    check state by stateFlow

--- app/src/main/java/net/sherafatpour/cryptocurrencylist/MainActivity.kt
+++ app/src/main/java/net/sherafatpour/cryptocurrencylist/MainActivity.kt
@@ -2,17 +2,61 @@ package net.sherafatpour.cryptocurrencylist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import net.sherafatpour.cryptocurrencylist.databinding.ActivityMainBinding
+import net.sherafatpour.cryptocurrencylist.viewModel.CryptoViewModel
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-
+    private val cryptoViewModel: CryptoViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
-       binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        cryptoViewModel.getCryptoCurrency(
+            vs_currency = "usd",
+            order = "market_cap_desc",
+            per_page = "20",
+            page = "1",
+            false
+        )
+
+        lifecycleScope.launchWhenStarted {
+            cryptoViewModel.cryptoStatus.collect { event ->
+                when (event) {
+                    is CryptoViewModel.CryptoStatus.Success -> {
+                        binding.progress.isVisible = false
+                        runOnUiThread {
+                            Toast.makeText(this@MainActivity, event.result.size.toString(), Toast.LENGTH_LONG).show()
+                            binding.text.text = event.result[0].name
+
+                        }
+
+
+                    }
+                    is CryptoViewModel.CryptoStatus.Failure ->{
+                        binding.progress.isVisible = false
+                        binding.text.text = event.error
+
+                    }
+                    is CryptoViewModel.CryptoStatus.Loading-> {
+                        binding.progress.isVisible = true
+
+
+                    }
+                    else -> Unit
+
+
+                }
+
+
+            }
+        }
 
 
     }
--- app/src/main/java/net/sherafatpour/cryptocurrencylist/viewModel/CryptoViewModel.kt
+++ app/src/main/java/net/sherafatpour/cryptocurrencylist/viewModel/CryptoViewModel.kt
@@ -1,13 +1,15 @@
 package net.sherafatpour.cryptocurrencylist.viewModel
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import net.sherafatpour.cryptocurrencylist.data.Repository
 import net.sherafatpour.cryptocurrencylist.data.models.CryptoModel
 import net.sherafatpour.cryptocurrencylist.util.DispatcherProvider
+import net.sherafatpour.cryptocurrencylist.util.Resource
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,7 +17,16 @@ class CryptoViewModel @Inject constructor(
   private val  repository: Repository,
   private val dispatcherProvider: DispatcherProvider
 ):ViewModel() {
-    var list: MutableLiveData<CryptoModel> = MutableLiveData<CryptoModel>()
+
+    sealed class CryptoStatus{
+        class Success(val result: CryptoModel):CryptoStatus()
+        class Failure(val error: String):CryptoStatus()
+        object Loading : CryptoStatus()
+        object Empty : CryptoStatus()
+    }
+
+    private val _crypto = MutableStateFlow<CryptoStatus>(CryptoStatus.Empty)
+    val cryptoStatus : StateFlow<CryptoStatus> = _crypto
 
     fun getCryptoCurrency(vs_currency: String,
                                 order: String,
@@ -24,9 +35,18 @@ class CryptoViewModel @Inject constructor(
                                 sparkline: Boolean){
 
         viewModelScope.launch(dispatcherProvider.io) {
+            _crypto.value = CryptoStatus.Loading
+            when(val response = repository.getCryptoList(vs_currency, order, per_page, page, sparkline)){
+                is Resource.Error -> _crypto.value = CryptoStatus.Failure(response.message!!)
+                is Resource.Success->{
+                    _crypto.value = CryptoStatus.Success(response.data!!)
 
-            val listResponse = repository.getCryptoList(vs_currency, order, per_page, page, sparkline)
-            list.postValue(listResponse.data!!)
+
+                }
+
+            }
+
+
 
         }
 
